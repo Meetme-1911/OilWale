1966d4be8f9c9ffdbc3e21aa9d75b2527ff3928f
LazyCROW <hardikkardam21@gmail.com>
2021-09-21 12:00:15 +0530

    overflow fix

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -24,119 +24,122 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Container(
           padding: EdgeInsets.all(48.0),
           child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.phone,
-                        color: Colors.deepOrange,
-                      ),
-                      hintText: '000-000-0000',
-                      labelText: 'Enter phone',
-                      labelStyle: TextStyle(color: Colors.deepOrange),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(
+              child: SingleChildScrollView(
+            reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.phone,
                           color: Colors.deepOrange,
                         ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepOrange)),
-                      hintStyle: TextStyle(color: Colors.deepOrange)),
+                        hintText: '000-000-0000',
+                        labelText: 'Enter phone',
+                        labelStyle: TextStyle(color: Colors.deepOrange),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: BorderSide(
+                            color: Colors.deepOrange,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepOrange)),
+                        hintStyle: TextStyle(color: Colors.deepOrange)),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.password,
-                        color: Colors.deepOrange,
-                      ),
-                      hintText: "Secret",
-                      labelText: 'Enter password',
-                      labelStyle: TextStyle(color: Colors.deepOrange),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide(
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.password,
                           color: Colors.deepOrange,
                         ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepOrange)),
-                      hintStyle: TextStyle(color: Colors.deepOrange)),
-                ),
-              ),
-              RadioListTile<Choice>(
-                title: const Text('Login as Customer'),
-                value: Choice.Customer,
-                groupValue: _choice,
-                onChanged: (Choice? value) {
-                  setState(() {
-                    _choice = value;
-                  });
-                },
-              ),
-              RadioListTile<Choice>(
-                title: const Text('Login as Garage Dealer'),
-                value: Choice.Garage,
-                groupValue: _choice,
-                onChanged: (Choice? value) {
-                  setState(() {
-                    _choice = value;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_choice == Choice.Customer) {
-                    Navigator.pushReplacementNamed(context, '/cust_home');
-                  } else if (_choice == Choice.Garage) {
-                    Navigator.pushReplacementNamed(context, '/garage_home');
-                  }
-                },
-                style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                        Size.fromWidth(MediaQuery.of(context).size.width))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 16.0),
+                        hintText: "Secret",
+                        labelText: 'Enter password',
+                        labelStyle: TextStyle(color: Colors.deepOrange),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: BorderSide(
+                            color: Colors.deepOrange,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepOrange)),
+                        hintStyle: TextStyle(color: Colors.deepOrange)),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  children: [
-                    Expanded(child: Divider()),
-                    Text("or"),
-                    Expanded(child: Divider())
-                  ],
+                RadioListTile<Choice>(
+                  title: const Text('Login as Customer'),
+                  value: Choice.Customer,
+                  groupValue: _choice,
+                  onChanged: (Choice? value) {
+                    setState(() {
+                      _choice = value;
+                    });
+                  },
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/cust_createAccount');
-                },
-                style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                        Size.fromWidth(MediaQuery.of(context).size.width))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Create New Customer Account',
-                    style: TextStyle(fontSize: 16.0),
+                RadioListTile<Choice>(
+                  title: const Text('Login as Garage Dealer'),
+                  value: Choice.Garage,
+                  groupValue: _choice,
+                  onChanged: (Choice? value) {
+                    setState(() {
+                      _choice = value;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_choice == Choice.Customer) {
+                      Navigator.pushReplacementNamed(context, '/cust_home');
+                    } else if (_choice == Choice.Garage) {
+                      Navigator.pushReplacementNamed(context, '/garage_home');
+                    }
+                  },
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(
+                          Size.fromWidth(MediaQuery.of(context).size.width))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
                   ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    children: [
+                      Expanded(child: Divider()),
+                      Text("or"),
+                      Expanded(child: Divider())
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/cust_createAccount');
+                  },
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(
+                          Size.fromWidth(MediaQuery.of(context).size.width))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Create New Customer Account',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                )
+              ],
+            ),
           )),
         ));
   }
